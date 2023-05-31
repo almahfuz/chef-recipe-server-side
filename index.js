cd59f07df71ae2392c35d9e49c49850794931276
almahfuz <abujafaralmahfuz@gmail.com>
2023-05-31 15:34:10 +0100

    chef items add

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 7000;
 
-const meal = require('./data/meal.json')
+// const meal = require('./data/meal.json')
+const meal = require('./data/chef.json')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('the Chef items watching here mood')
 })
-app.get('/meal', (req, res) => {
-    res.send(meal);
-  })
+
+app.get('/chef', (req, res) => {
+  res.send(meal);
+})
 
 app.listen(port, () => {
   console.log(`Chef api running ${port}`)
