96f730b4a8f486df6cf3c7c1ae5a06ade2fdd88a
almahfuz <abujafaralmahfuz@gmail.com>
2023-06-05 14:25:46 +0100

    modified the code

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 7000;
 
-// const meal = require('./data/meal.json')
-const chefMeal = require('./data/chef.json')
 app.use(cors());
+
+const chefs = require('./data/chef.json')
+
 app.get('/', (req, res) => {
-  res.send('the Chef items watching here mood')
+  res.json({message:"Chef items watching mood"})
 })
 
-app.get('/chef', (req, res) => {
-  res.send(chefMeal);
+app.get('/AllData', (req, res) => {
+  res.send(chefs);
 })
+
+app.get("/AllData/:id", (req, res) => {
+  const _id = req.params.id;
+  const singleChefs = chefs.find((chef) => +chef.id === +_id);
+  res.send(singleChefs);
+});
 
 app.listen(port, () => {
   console.log(`Chef api running ${port}`)
